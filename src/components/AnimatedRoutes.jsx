52b85cc021aeceab5fe11b9243322bcb7951c0ab
kennedyifeadi <kennedyifeadi@gmail.com>
2024-12-05 02:22:49 +0100

    Refactor AnimatedRoutes component to include framer-motion animations and update route paths

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,9 +1,22 @@
-
+import { Route, Routes, useLocation } from "react-router-dom"
+import { AnimatePresence } from "framer-motion"
+import Dashboard from "../pages/Dashboard";
+import PageNotFound from "../pages/PageNotFound";
+import DataVisualization from "../pages/DataVisualization";
+import AiSummary from "../pages/AiSummary";
 export const AnimatedRoutes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const location = useLocation();
+    return (
+        <div className="w-full h-full">
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Dashboard/>} exact />
+                <Route path="/datavisualization" element={<DataVisualization/>} />
+                <Route path="/aisummary" element={<AiSummary/>} />
+                <Route path="*" element={<PageNotFound/>} /> 
+            </Routes>
+      </AnimatePresence>
+      </div>
+    )
 }
 
