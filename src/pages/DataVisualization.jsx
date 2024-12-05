e157ce6a66cc90276451cb6228377c518b4c0c71
kennedyifeadi <kennedyifeadi@gmail.com>
2024-12-05 02:27:40 +0100

    Refactor page components to include framer-motion animations

--- src/pages/DataVisualization.jsx
+++ src/pages/DataVisualization.jsx
@@ -1,10 +1,16 @@
+import { motion } from "framer-motion"
 
 const DataVisualization = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <motion.div 
+            className="h-full w-full flex flex-col"
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            exit={{opacity:0}}
+            >
+            <h1 className="text-3xl font-bold text-center mt-10">Data Visualization</h1>
+            </motion.div>
+      )
 }
 
 export default DataVisualization
