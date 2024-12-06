09d1be1eec68f1c5872d41978f6258d3baf850ba
kennedyifeadi <kennedyifeadi@gmail.com>
2024-12-07 00:18:35 +0100

    Refactor App.jsx to add Firebase messaging and toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,27 @@ import { BrowserRouter } from "react-router-dom"
 import { NavBar } from "./components/nav"
 import { AnimatedRoutes } from "./components/AnimatedRoutes"
 import { useEffect } from "react"
-import { generateToken } from "./notification/firebase"
+import { generateToken, messaging } from "./notification/firebase"
+import { onMessage } from "firebase/messaging"
+import toast, { Toaster } from 'react-hot-toast';
 
 
 function App() {
 
   useEffect(() => {
     generateToken();
-  })
+    onMessage(messaging, (payload) => {
+      console.log("Message received. ", payload);
+      toast(payload.notification.body);
+    });
+  }, [])
 
   return (
       <BrowserRouter>
         <div className='w-full h-[110dvh] scroll-smooth pb-[5rem] md:pb-0 md:h-[100dvh] relative flex flex-col overflow-x-hidden'>
           <NavBar/>
           <AnimatedRoutes/>
+          <Toaster />
         </div>
       </BrowserRouter>
   )
