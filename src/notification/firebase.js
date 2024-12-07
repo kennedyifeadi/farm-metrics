088b1746c606b78714d1b388189ad4d2b230ea12
kennedyifeadi <kennedyifeadi@gmail.com>
2024-12-07 15:48:05 +0100

    Refactor firebase.js to use environment variables for Firebase configuration

--- src/notification/firebase.js
+++ src/notification/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken} from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVVOZ1qECCpEAYbqtY2n0tTRgHXTRSiYo",
-  authDomain: "farm-metrics-a48d3.firebaseapp.com",
-  projectId: "farm-metrics-a48d3",
-  storageBucket: "farm-metrics-a48d3.firebasestorage.app",
-  messagingSenderId: "167456631149",
-  appId: "1:167456631149:web:e5cd283dc5feb33ecb9264",
-  measurementId: "G-J4SR7W9X96"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
