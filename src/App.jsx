45ee85a44c3e8fdaf72d82ca26700dcd7ad368b3
kennedyifeadi <kennedyifeadi@gmail.com>
2025-01-21 19:41:16 +0100

    Refactor firebase-messaging-sw.js and App.jsx
    Remove commented out code and unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import { BrowserRouter } from "react-router-dom"
 import { NavBar } from "./components/nav"
 import { AnimatedRoutes } from "./components/AnimatedRoutes"
 import { useEffect } from "react"
-import { generateToken, messaging } from "./notification/firebase"
-import { onMessage } from "firebase/messaging"
-import toast, { Toaster } from 'react-hot-toast';
+// import { generateToken, messaging } from "./notification/firebase"
+// import { onMessage } from "firebase/messaging"
+// import toast, { Toaster } from 'react-hot-toast';
 
 
 function App() {
 
-  useEffect(() => {
-    generateToken();
-    onMessage(messaging, (payload) => {
-      console.log("Message received. ", payload);
-      toast(payload.notification.body);
-    });
-  }, [])
+  // useEffect(() => {
+  //   generateToken();
+  //   onMessage(messaging, (payload) => {
+  //     console.log("Message received. ", payload);
+  //     toast(payload.notification.body);
+  //   });
+  // }, [])
 
   return (
       <BrowserRouter>
         <div className='w-full h-max scroll-smooth md:pb-0 md:h-[100dvh] relative flex flex-col overflow-auto'>
           <NavBar/>
           <AnimatedRoutes/>
-          <Toaster />
+          {/* <Toaster /> */}
         </div>
       </BrowserRouter>
   )
