c835be4322acc255acce52a5c37b1349e6bc713c
kennedyifeadi <kennedyifeadi@gmail.com>
2025-01-29 20:00:34 +0100

    Refactor AiSummaryComp component to improve styling and readability

--- src/components/AiSummaryComp.jsx
+++ src/components/AiSummaryComp.jsx
@@ -2,18 +2,18 @@ import PropTypes from 'prop-types';
 
 export const AiSummaryComp = ({title, text, button, chart}) => {
   return (
-    <div className='w-[90%] md:w-[28%] h-[80%] bg-[#0a0a0a] flex flex-col rounded-lg p-4 justify-between'>
-      <span className='w-full text-white text-[24px] font-semibold'>
+    <div className='w-[90%] md:w-[28%] h-[80%] bg-white shadow-md flex flex-col rounded-lg p-4 justify-between'>
+      <span className='w-full text-[#0a0a0a] text-[24px] font-semibold'>
         {title}
       </span>
-      <span className="w-full text-white text-[15px]">
+      <span className="w-full text-[#0a0a0a] text-[15px]">
         {text}
       </span>
       <div className='w-full h-max mb-4'>
       {chart}
       </div>
       <span className='w-full h-[30px] flex justify-center'>
-        <button className='w-[85%] h-full bg-white rounded-full text-[#0a0a0a] flex justify-center items-center'>
+        <button className='w-[85%] h-full bg-[#0a0a0a] shadow-md rounded-full text-white flex justify-center items-center'>
           {button}
         </button>
       </span>
